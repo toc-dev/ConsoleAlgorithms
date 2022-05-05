272dc3f296a423c6c5a25cef3c934eaefd2a9ca6
Tochukwu <tochukwusage4@gmail.com>
2022-05-05 21:29:06 +0100

    More Algos

--- ConsoleAlgorithms/CodeWarsKata.cs
+++ ConsoleAlgorithms/CodeWarsKata.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ConsoleAlgorithms
 {
@@ -108,9 +109,280 @@ namespace ConsoleAlgorithms
 
                 return n == 0 ? new double[] { 0 } : res;
             }
+            
+            public double[] Tribonacci(double[] a, int n)
+            {
+                var res = new System.Collections.Generic.List<double>(a);
+                for (int i = 3; i < n; i++)
+                    res.Add(res[i - 1] + res[i - 2] + res[i - 3]);
+                return (n == 0) ? new double[] { 0.0d } : res.GetRange(0, n).ToArray();
+            }
             */
         }
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            List<string> anagrams = new List<string>();
+            char[] wd = word.ToCharArray();
+            foreach (string w in words)
+            {
+                char[] x = w.ToCharArray();
+                var xNotWd = x.Except(wd).ToList();
+                var wdNotX = wd.Except(x).ToList();
 
+                var compare = !wdNotX.Any() && !xNotWd.Any();
+
+                if (compare == true && x.Length == wd.Length)
+                {
+                    anagrams.Add(w);
+                }
+            }
+            return anagrams;
+            /*
+            var pattern = word.OrderBy(p => p).ToArray();
+            return words.Where(item => item.OrderBy(p => p).SequenceEqual(pattern)).ToList();
+            */
+        }
+        public static bool IsPangram(string str)
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            char[] lettersList = letters.ToUpper().ToCharArray();
+            for (int i = 0; i <26; i++)
+            {
+                Console.WriteLine(lettersList[i]);
+            }
+            char[] arrayToString = str.ToUpper().ToCharArray();
+            Array.Sort(arrayToString);
+            var arrayInAZ = lettersList.Except(arrayToString); //basically checking the first list against the second list, and printing out the stuff in the first list that is not in the second list
+            Console.WriteLine(arrayInAZ);
+            foreach (char i in arrayInAZ)
+            {
+                Console.WriteLine(i);
+            }
+            //return str.Where(ch => Char.IsLetter(ch)).Select(ch => Char.ToLower(ch)).Distinct().Count() == 26;
+
+            return !arrayInAZ.Any();
+        }
+
+        public static bool comp(int[] a, int[] b)
+        {
+            // your code
+            if ((a?.Length != 0 && b?.Length == 0) || a?.Length != b?.Length)
+                return false;
+
+            int[] c = new int[a.Count()];
+            bool[] e = new bool[a.Count()];
+            bool[] d = new bool[a.Count()];
+            Array.Sort(a);
+            Array.Sort(b);
+            for (int i = 0; i < a.Count(); i++)
+            {
+                c[i] = c[i] = (a[i] * a[i]);
+                e[i] = true;
+                if (c[i] == b[i])
+                {
+                    d[i] = true;
+                }
+                else
+                    d[i] = false;
+            }
+            if (d.Count() != e.Count())
+                return false;
+            var check = d.Except(e);
+            //var check2 = c.Except(b);
+            return !check.Any();
+            /*
+            public static bool comp(int[] a, int[] b)
+            {
+                if ((a == null) || (b == null)) return false;
+                int[] aa = a.Select(s => s * s).ToArray();
+                Array.Sort(aa);
+                Array.Sort(b);
+                return aa.SequenceEqual(b);
+            }
+            */
+            /*
+            public static bool comp(int[] a, int[] b)
+            {
+                return a == null || b == null ? false : a.Sum() == b.Sum(x => Math.Sqrt(x));
+            }*/
+
+        }
+        public static string OrderHackedSolution(string words)
+        {
+            List<string> wordList = words.Split(" ").ToList();
+            List<char> moreWords = new List<char>();
+            
+            char[] numList = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            //var wordd = moreWords.OrderBy(p => p);
+            List<string> wordOrder = new List<string>();
+
+            while(wordOrder.Count < wordList.Count)
+            {
+                foreach (var i in numList)
+                {
+                    foreach (var word in wordList)
+                    {
+                        moreWords = word.ToCharArray().ToList();
+                        foreach (var w in moreWords)
+                        {
+                            if (w == i)
+                            {
+                                wordOrder.Add(word);
+                            }
+                        }
+                    }
+                }
+            }
+            
+            return string.Join(" ", wordOrder);//organizedWords.Remove(organizedWords.Length - 1, 1).ToString(); //string.Join(" ", organizedWords);
+        }
+        
+        public static string Order(string words)
+        {
+            /*
+            List<string> wordList = words.Split(" ").ToList();
+            List<char> moreWords = new List<char>();//words.Split(" ").ToList();
+            foreach (var word in wordList)
+            {
+                moreWords = word.ToCharArray().ToList();
+            }
+            var isDigit = moreWords.Where(x => char.IsDigit(x)).Any();
+            */
+            List<string> wordList = words.Split(" ").ToList();
+            string[] arrangedWords = new string[words.Length];
+            foreach (string word in wordList)
+            {
+                string number = string.Concat(word.Where(char.IsNumber)).Trim();
+                int num;
+                bool isInteger = int.TryParse(number, out num);
+                if (isInteger == true)
+                {
+                    arrangedWords[num] = word;
+                }
+            }
+            return string.Join(" ", arrangedWords).Trim();
+
+            //return string.Join(" ", words.Split().OrderBy(w => w.SingleOrDefault(char.IsDigit)));
+        }
+        public static int Solution(int value)
+        {
+            int a = 0;
+            // Magic Happens
+            for (int i = 0; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    a += i;
+                }
+            }
+            return a;
+
+            //return Enumerable.Range(0, n).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
+        }
+        public static string BreakCamelCase(string str)
+        {
+            //complete the function
+            //return string.Join("", )
+
+            var strings = (from s in str.ToCharArray()
+                          where char.IsUpper(s)
+                          select string.Join($" {s}", str.Split(s))).ToArray();
+            
+            foreach (var i in strings)
+            {
+                Console.WriteLine(i);
+            }
+            
+
+            return string.Join(" ", strings);
+            //return string.Concat(str.Select(c => Char.IsUpper(c) ? " " + c : c.ToString()));
+        }
+        public static bool IsPrime(int n)
+        {
+            // TODO
+            int floor = (int)(Math.Floor(Math.Sqrt(n)));
+            if (n == 2)
+            {
+                return true;
+            }
+            if (n < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= floor; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+
+        }
+        public static string SpinWords(string sentence)
+        {
+            
+            List<string> sentenceList = sentence.Split(" ").ToList();
+            Console.WriteLine(sentenceList);
+
+            for (int i = 0; i < sentenceList.Count(); i++)
+            {
+                StringBuilder reversedWord = new StringBuilder();
+                char[] splitWord = sentenceList[i].ToCharArray();
+                Console.WriteLine(splitWord.Length);
+                if (splitWord.Length >= 5)
+                {
+                    for (int j = splitWord.Length - 1; j > -1; j--)
+                    {
+                        reversedWord.Append(splitWord[j].ToString());
+                    }
+                    sentenceList[i] = reversedWord.ToString();
+                }
+                
+            }
+            return string.Join(" ", sentenceList);
+
+            //return String.Join(" ", sentence.Split(' ').Select(str => str.Length >= 5 ? new string(str.Reverse().ToArray()) : str));
+        }
+
+        public static int FindUnsortedSubarray(int[] nums)
+        {
+            //From LeetCode. Find the smallest unsorted subarray.
+            if (nums.Count() < 2)
+            {
+                return 0;
+            }
+
+            List<int> numbers = nums.ToList();
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+                if (numbers[i] == numbers.Min())
+                {
+                    numbers.RemoveAt(i--);
+                }
+            
+                else
+                {
+                    break;
+                }
+                
+            }
+            for (int n = numbers.Count() - 1; n > -1; n--)
+            {
+                if (numbers[n] == numbers.Max())
+                {
+                    numbers.RemoveAt(n);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            
+            return numbers.Count();
+        }
     }
 }
 
--- ConsoleAlgorithms/Program.cs
+++ ConsoleAlgorithms/Program.cs
@@ -3,8 +3,8 @@ using ConsoleAlgorithms;
 
 Console.WriteLine("Hello, World!");
 CodeWarsKata codeWarsKata = new CodeWarsKata();
-var list = CodeWarsKata.GetIntegersFromList(new List<object>() { 1, 2, "a", "b" });
-Console.WriteLine(list);
+//var list = CodeWarsKata.GetIntegersFromList(new List<object>() { 1, 2, "a", "b" });
+//Console.WriteLine(list);
 /*
 int[][] jArray = new int[1][]
 {
@@ -13,6 +13,7 @@ int[][] jArray = new int[1][]
 */
 //var openSenior = CodeWarsKata.OpenOrSenior((jArray);
 //Console.WriteLine(op);
+/*
 string[] walk = { "n", "s", "e", "w" };
 Console.WriteLine(CodeWarsKata.IsValidWalk(walk));
 double[] signature = {13,15,19}; //{5, 1, 1, 1, 3};
@@ -21,3 +22,35 @@ foreach (int i in two)
 {
     Console.WriteLine(i);
 }
+string word = "abba";
+var wordd = "laser";
+List<string> words = new List<string> { "aabb", "abcd", "bbaa", "dada"};
+List<string> moreWords = new List<string> {"lazing", "lazy", "lacer", "lasser"};
+
+var ana = CodeWarsKata.Anagrams(wordd, moreWords);
+foreach(string s in ana)
+{
+    Console.WriteLine(s);
+}
+string wordy = "abcdefghijklmnopqrstuvwnandnsnyzx";
+Console.WriteLine(CodeWarsKata.IsPangram(wordy));
+*/
+/*
+int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+Console.WriteLine(CodeWarsKata.comp(a, b) );
+
+string ap = "4of Fo1r pe6ople g3ood th5e the2"; //"is2 Thi1s T4est 3a";
+//Console.WriteLine(CodeWarsKata.Order(ap));
+Console.WriteLine(CodeWarsKata.Order(ap));
+string cC = "camelCasingTest";
+Console.WriteLine(CodeWarsKata.BreakCamelCase(cC));
+*/
+string w = "Ah finesse if I Welcome";
+Console.WriteLine(CodeWarsKata.SpinWords(w));
+
+int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+int[] b = new int[] {4, 121, 144, 19, 161, 19, 19, 11, 900 };
+int[] c = new int[] { 1, 2, 3, 4, 5,4, 5, 6,8,9 };
+
+Console.WriteLine(CodeWarsKata.FindUnsortedSubarray(c));
